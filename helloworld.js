4137902cac20ea04005d10dfd8e4c30c1867b445
Glenn Goodrich <glenn.goodrich@gmail.com>
2018-05-31 13:20:31 -0400

    test

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,6 @@ var http = require("http")
 
 http.createServer(function (request, response) {
 
-   console.log("arse");
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
